a9283b6061ac7a64bd21a783cfddcb23828104f7
Weilu Gong <whatsthepoint02@gmail.com>
2014-12-23 23:10:35 -0800

    picA added

--- Blog.js
+++ Blog.js
@@ -21,38 +21,48 @@
 });
 
 $(document).ready(function(){
-	$('#scroll').click(function(){
-		$('html,body').animate({
-			scrollTop:0
-		});
+  $('#scroll').click(function(){
+	$('html,body').animate({
+	  scrollTop:0
 	});
+  });
 });
 
 $(window).scroll(function(){
-	if($(window).scrollTop() > 200){
-		$('#scroll').show()
-	}
-	else{
-		$('#scroll').hide()
-	}
+  if($(window).scrollTop() > 200){
+	$('#scroll').show()
+  }
+  else{
+	$('#scroll').hide()
+  }
 });
 
 $(document).ready(function(){
-	var status = 0;
-	$('.latest img').click(function(){
-	    if(status === 0){
-	    	$(this).animate({
-		        height:'+=15px',
-		        width:'+=15px'
-		    });
-		    status = 1
-		}
-		else{
-			$(this).animate({
-		        height:'-=15px',
-		        width:'-=15px'
-		    });
-		    status = 0
-		};
-	});
+  var status = 0;
+  $('.latest img').click(function(){
+	if(status === 0){
+	  $(this).animate({
+		height:'+=15px',
+		width:'+=15px'
+	  });
+	  status = 1
+	}
+	  else{
+		$(this).animate({
+		  height:'-=15px',
+		  width:'-=15px'
+		});
+		  status = 0
+	  };
+  });
+});
+
+$(document).ready(function(){
+  $('.guide').mouseenter(function(){
+  	$('.guide-menu').slideDown()
+  });
+  $('.guide').mouseleave(function(){
+  	$('.guide-menu').slideUp()
+  });
+
 });
